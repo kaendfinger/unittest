f7145ebdbb1919d77700a8eb61130224202da2a4
Kevin Moore <kevmoo@google.com>
2015-02-21 10:39:59 -0800

    Remove deprecated classes and members

--- lib/unittest.dart
+++ lib/unittest.dart
@@ -8,12 +8,10 @@ import 'dart:async';
 
 import 'package:path/path.dart' as p;
 
-import 'src/configuration.dart';
 import 'src/declarer.dart';
 import 'src/console_reporter.dart';
 import 'src/invoker.dart';
 import 'src/suite.dart';
-import 'src/test_case.dart';
 
 export 'package:matcher/matcher.dart'
     hide
@@ -43,13 +41,10 @@ export 'package:matcher/matcher.dart'
         throwsUnimplementedError,
         throwsUnsupportedError;
 
-export 'src/configuration.dart';
 export 'src/expect.dart';
 export 'src/expect_async.dart';
 export 'src/future_matchers.dart';
 export 'src/prints_matcher.dart';
-export 'src/simple_configuration.dart';
-export 'src/test_case.dart';
 export 'src/throws_matcher.dart';
 export 'src/throws_matchers.dart';
 
@@ -102,76 +97,3 @@ void handleExternalError(error, String message, [stackTrace]) {
 /// Registers an exception that was caught for the current test.
 void registerException(error, [StackTrace stackTrace]) =>
     Invoker.current.handleError(error, stackTrace);
-
-// What follows are stubs for various top-level names supported by unittest
-// 0.11.*. These are preserved for the time being for ease of migration, but
-// should be removed before this is released as stable.
-
-@deprecated
-typedef dynamic TestFunction();
-
-@deprecated
-Configuration unittestConfiguration = new Configuration();
-
-@deprecated
-bool formatStacks = true;
-
-@deprecated
-bool filterStacks = true;
-
-@deprecated
-String groupSep = ' ';
-
-@deprecated
-void logMessage(String message) => print(message);
-
-@deprecated
-final testCases = [];
-
-@deprecated
-const int BREATH_INTERVAL = 200;
-
-@deprecated
-TestCase get currentTestCase => null;
-
-@deprecated
-const PASS = 'pass';
-
-@deprecated
-const FAIL = 'fail';
-
-@deprecated
-const ERROR = 'error';
-
-@deprecated
-void skip_test(String spec, TestFunction body) {}
-
-@deprecated
-void solo_test(String spec, TestFunction body) => test(spec, body);
-
-@deprecated
-void skip_group(String description, void body()) {}
-
-@deprecated
-void solo_group(String description, void body()) => group(description, body);
-
-@deprecated
-void filterTests(testFilter) {}
-
-@deprecated
-void runTests() {}
-
-@deprecated
-void ensureInitialized() {}
-
-@deprecated
-void setSoloTest(int id) {}
-
-@deprecated
-void enableTest(int id) {}
-
-@deprecated
-void disableTest(int id) {}
-
-@deprecated
-withTestEnvironment(callback()) => callback();
